083050d4df0f1c29cd6fe91637e40d4ca90eafd1
joice <joydingforever@gmail.com>
2022-07-20 17:28:08 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+class Main {
+    public static void main(String[] args) {
+
+        String[] javaNoobi = { "happy", "cute", "nice", "angry"};
+
+        // Uses loops through index
+        for (int i = 0; i < javaNoobi.length; i++) {
+
+            System.out.println(javaNoobi[i]);
+        }
+
+        // Uses loops directly to print strings
+        // for (String i : javaNoobi) {
+
+        //   System.out.println(i);
+        // }
+
+    }
+}
